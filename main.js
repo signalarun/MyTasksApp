8b50effba6237c3c1994760a3a25b1c683551af2
signalarun <signalarun@gmail.com>
2021-07-21 14:55:28 +0530

    New import

---
+++ main.js
@@ -0,0 +1,47 @@
+/* 
+   Modules to control application life and create native browser window
+   In this file you can include the rest of your app's specific main
+   process code. You can also put them in separate files and
+   require them here.
+*/
+const { app, BrowserWindow, ipcMain, Notification } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      //enableRemoteModule: true
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.setMenuBarVisibility(false)
+  win.loadFile('main.html')
+}
+
+app.whenReady().then(() => {
+
+  createWindow()
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+ipcMain.handle('show-notification', (event, ...args) => {
+  const notification = {
+    title: 'New Task',
+    body: `Added: ${args[0]}`
+  }
+
+  new Notification(notification).show()
+});
